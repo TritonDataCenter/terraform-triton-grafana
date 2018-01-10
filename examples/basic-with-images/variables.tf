b0839f78d638ab04f4b8993028883bcbaba7fc3a
Cameron Stokes <cameron@cameronstokes.com>
2018-01-09 16:15:19 -0800

    Remove unsused variables images example.

--- examples/basic-with-images/variables.tf
+++ examples/basic-with-images/variables.tf
@@ -5,23 +5,3 @@ variable "private_key_path" {
   description = "The path to the private key to use for provisioning machines."
   type        = "string"
 }
-
-variable "prometheus_cmon_cert_file_path" {
-  description = <<EOF
-The path to the TLS certificate to use for authentication to the CMON endpoint.
-The sdc-docker setup script is the easiest way to obtain this -
-https://raw.githubusercontent.com/joyent/sdc-docker/master/tools/sdc-docker-setup.sh.
-EOF
-
-  type = "string"
-}
-
-variable "prometheus_cmon_key_file_path" {
-  description = <<EOF
-The path to the TLS key to use for authentication to the CMON endpoint.
-The sdc-docker setup script is the easiest way to obtain this -
-https://raw.githubusercontent.com/joyent/sdc-docker/master/tools/sdc-docker-setup.sh.
-EOF
-
-  type = "string"
-}
